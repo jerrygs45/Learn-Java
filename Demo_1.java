12ec43d2fcf4717630689b731b091b1104dc001e
Ratna Srijan <srijanrat123@gmail.com>
2025-06-04 14:50:27 +0530

    adding demo class

---
+++ Demo_1.java
@@ -0,0 +1,11 @@
+class Demo_1 {
+
+    public static void main(String []args) {
+        System.out.print("Hello ");     
+        System.out.print("world!\n");
+        System.out.println("Hello");
+        System.out.println("world!");
+    }
+}
+
+ 
