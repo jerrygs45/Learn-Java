4de1061be480f6e0a799377c14a7cefe42e30c05
Ratna Srijan <srijanrat123@gmail.com>
2025-06-04 15:19:11 +0530

    adding variables java file

---
+++ Variables_2.java
@@ -0,0 +1,10 @@
+public class Variables_2 {
+    public static void main(String[] args) {
+        int age; // this is variable declaration.
+        age=22; //This is a variable assignment. When assignment is done at declaration its called initialization.
+        double bodyFat= 10.4;  // thiis is a variable initialization.
+        System.out.println("My age is: " + age);
+        System.out.println("Your body fat is: " + bodyFat);
+    }
+    
+}
